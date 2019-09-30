86301cc967f8c75c34af39f568ef65a4521f5e5d
cellson7170 <55819943+cellson7170@users.noreply.github.com>
2019-09-30 09:10:12 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+//HEADER
+//Program Name: HelloWorld
+//Author: Chris Ellson
+//Class: CS260 Fall 2019
+//Date: 9/30/2019
+//Description: This simple program will print "Hello World!" to the console.
+
+public class HelloWorld {
+//Declaring the class "HelloWorld"
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// This is our main method. It will return and print "Hello World!"
+
+	}
+
+}
+
+/* FOOTER
+
+Hello World!
+
+*/
